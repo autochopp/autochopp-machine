c88042fa04365c8dce5e3c843eb17e49b68c4b53
Filipe Ribeiro <filiperibeirom7@gmail.com>
2017-10-19 19:36:12 -0200

    integrating kivy with qr code reader
    Signed-off-by: Filipe Ribeiro <filiperibeirom7@gmail.com>

--- connected.py
+++ connected.py
@@ -1,8 +1,40 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, SlideTransition
+import zbar
+
+from PIL import Image
+import cv2
 
 class Connected(Screen):
-    def disconnect(self):
-        self.manager.transition = SlideTransition(direction="right")
-        self.manager.current = 'login'
-        self.manager.get_screen('login')
+    def readcode():
+        # Begin capturing video. You can modify what video source to use with VideoCapture's argument. It's currently set
+        # to be your webcam.
+        capture = cv2.VideoCapture(0)
+
+        while True:
+            # To quit this program press q.
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+            # Breaks down the video into frames
+            ret, frame = capture.read()
+
+            # Displays the current frame
+            cv2.imshow('Current', frame)
+
+            # Converts image to grayscale.
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Uses PIL to convert the grayscale image into a ndary array that ZBar can understand.
+            image = Image.fromarray(gray)
+            width, height = image.size
+            zbar_image = zbar.Image(width, height, 'Y800', image.tostring())
+
+            # Scans the zbar image.
+            scanner = zbar.ImageScanner()
+            scanner.scan(zbar_image)
+
+            # Prints data from image.
+            for decoded in zbar_image:
+                print(decoded.data)
+
